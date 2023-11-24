448ba419958e23585b62892b77a7b1fe1ce2448f
Turtogtokh-Altangerel <keatonturkuush.ta.ta@gmail.com>
2023-11-24 14:36:20 +0100

    add black formatter dependency

--- src/main_module/dummy.py
+++ src/main_module/dummy.py
@@ -1,17 +1,20 @@
 import pandas as pd
 
+
 def main():
     # Create a DataFrame
-    data = {'Name': ['Alice', 'Bob', 'Charlie'],
-            'Age': [25, 30, 35],
-            'City': ['New York', 'San Francisco', 'Los Angeles']}
-    
+    data = {
+        "Name": ["Alice", "Bob", "Charlie"],
+        "Age": [25, 30, 35],
+        "City": ["New York", "San Francisco", "Los Angeles"],
+    }
+
     df = pd.DataFrame(data)
 
     # Display the DataFrame
     print("DataFrame:")
     print(df)
 
+
 if __name__ == "__main__":
     main()
-
