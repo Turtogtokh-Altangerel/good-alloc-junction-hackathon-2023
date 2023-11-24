27acab6e377c71a23b286b50a1060eb4a3aee846
Turtogtokh-Altangerel <keatonturkuush.ta.ta@gmail.com>
2023-11-24 19:43:29 +0100

    yolo dummy files re-commit2

--- src/main_module/yolo.py
+++ src/main_module/yolo.py
@@ -1,7 +1,12 @@
 from ultralytics import YOLO
+import torch
 
 pathToData = "/Users/bp512/Desktop/junction-hackathon/project_root/good_alloc_junction_project/dataset/data.yaml"
 
 model = YOLO("yolov8n.pt")
-model.train(data=pathToData, epochs=1)
+
+if torch.cuda.device_count() > 1:
+    print("Using", torch.cuda.device_count(), "GPUs for training.")
+
+model.train(data=pathToData, epochs=1, device=0)
 metrics = model.val()
