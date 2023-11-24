18e6d8b550088d731f7d87f61a87fa9459c650f0
Turtogtokh-Altangerel <keatonturkuush.ta.ta@gmail.com>
2023-11-24 21:00:47 +0100

    find path for the trained model dynamically

--- src/main_module/yolo_custom.py
+++ src/main_module/yolo_custom.py
@@ -1,6 +1,8 @@
+import os
 from ultralytics import YOLO
 
-pathToBest = "RealOrFake.pt"
+pathToBest = os.path.join(os.path.dirname(os.path.abspath(__file__)), "RealOrFake.pt")
 model = YOLO(pathToBest)
 
-results = model(source=1, show=True, conf=0.1, save=True)
+
+results = model(source=0, show=True, conf=0.3, save=True)
