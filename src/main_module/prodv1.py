77a9fffbaa5aa58a541e54997f7229f0a1de9aa2
Namnansuren <namnansuren.buuveibaatar@gmail.com>
2023-11-25 14:32:07 +0100

    Added argparse in prodv1.py

--- src/main_module/prodv1.py
+++ src/main_module/prodv1.py
@@ -1,52 +1,62 @@
 from utility_module.extract_frames import extract_frames
 from ultralytics import YOLO
 from torch import stack
+import argparse
 
-# CONFIG
-path_to_glitch_detector = "src/resources/models/glitch_detector_model.pt"
-path_to_bad_guy_detector = "src/resources/models/best.pt"
-path_to_input_video = "/Users/bp512/Downloads/batuk_fake_3.mp4"
 
-glitch_detector_conf_rate = 0.4
-bad_guy_conf_values_mean_threshold = 0.75
+def main ():
+    parser = argparse.ArgumentParser(description="Deepfake detection script")
+    parser.add_argument("-f", "-file", help="Path to the video file", required=True)
+    args = parser.parse_args()
+    # CONFIG
+    path_to_glitch_detector = "src/resources/models/glitch_detector_model.pt"
+    path_to_bad_guy_detector = "src/resources/models/best.pt"
+    path_to_input_video = args.input_file
 
-# extract frames
-frames = extract_frames(path_to_input_video, num_frames=60)
+    glitch_detector_conf_rate = 0.4
+    bad_guy_conf_values_mean_threshold = 0.75
 
-# initialize
-glitch_detector = YOLO(path_to_glitch_detector)
-police = YOLO(path_to_bad_guy_detector)
+    # extract frames
+    frames = extract_frames(path_to_input_video, num_frames=60)
 
-# prediction
-print("Started deepfake prediction\n")
-results_police = police.predict(source=frames, verbose=False)
-results_glitch = glitch_detector.predict(
-    source=frames, conf=glitch_detector_conf_rate, verbose=False
-)
+    # initialize
+    glitch_detector = YOLO(path_to_glitch_detector)
+    police = YOLO(path_to_bad_guy_detector)
 
-# post-processing #1
-print("Evaluating results")
-bad_guy_conf_values_mean = (
-    stack([result.probs.data[0] for result in results_police]).mean().item()
-)
-if bad_guy_conf_values_mean > bad_guy_conf_values_mean_threshold:
-    print("BAD GUY DETECTED!")
-    quit()
+    # prediction
+    print("Started deepfake prediction\n")
+    results_police = police.predict(source=frames, verbose=False)
+    results_glitch = glitch_detector.predict(
+        source=frames, conf=glitch_detector_conf_rate, verbose=False
+    )
 
-# post-processing #2
-bad_hand_count = 0
-
-for result in results_glitch:
-    label_conf_dict = {
-        label: conf
-        for label, conf in zip(result.boxes.cls.tolist(), result.boxes.conf.tolist())
-    }
-
-    if label_conf_dict.get(1, 0) > 0.5:
-        bad_hand_count += 1
-    else:
-        bad_hand_count = 0
-
-    if bad_hand_count > 3:
-        print("Too many bad hand count!")
+    # post-processing #1
+    print("Evaluating results")
+    bad_guy_conf_values_mean = (
+        stack([result.probs.data[0] for result in results_police]).mean().item()
+    )
+    if bad_guy_conf_values_mean > bad_guy_conf_values_mean_threshold:
+        print("BAD GUY DETECTED!")
         quit()
+
+    # post-processing #2
+    bad_hand_count = 0
+
+    for result in results_glitch:
+        label_conf_dict = {
+            label: conf
+            for label, conf in zip(result.boxes.cls.tolist(), result.boxes.conf.tolist())
+        }
+
+        if label_conf_dict.get(1, 0) > 0.5:
+            bad_hand_count += 1
+        else:
+            bad_hand_count = 0
+
+        if bad_hand_count > 3:
+            print("Too many bad hand count!")
+            quit()
+
+
+if __name__ == "__main__":
+    main()
