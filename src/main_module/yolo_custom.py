271587b69d0654915659b87949f190defb5e3ba9
Turtogtokh-Altangerel <keatonturkuush.ta.ta@gmail.com>
2023-11-25 18:02:50 +0100

    big clean up and force state

--- src/main_module/yolo_custom.py
+++
@@ -1,8 +0,0 @@
-import os
-from ultralytics import YOLO
-
-pathToBest = os.path.join(os.path.dirname(os.path.abspath(__file__)), "RealOrFake.pt")
-model = YOLO(pathToBest)
-
-
-results = model(source=0, show=True, conf=0.3, save=True)
